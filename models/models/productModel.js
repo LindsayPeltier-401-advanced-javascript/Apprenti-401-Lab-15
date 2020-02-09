80f8c14f0f63668a64a421ebcf70842e7513deb7
LindsayPeltier <lindsaypeltier@gmail.com>
2020-02-08 20:40:41 -0800

    models

---
+++ models/models/productModel.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Model = require('./mongoModel');
+const schema = require('..schema/productSchema');
+/**
+ * @class
+ */
+class Product extends Model {
+  constructor() {
+    super(schema);
+  }
+}
+
+module.exports = Product;
